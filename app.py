75611a9f2942e0d5f426d45d76437f54396fce97
Tamerlan Ordukhanov <tamerlan.ord2005@gmail.com>
2025-12-30 02:15:20 +0400

    feat: add create()

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, session
+from datetime import datetime
 import sqlite3
 
 app = Flask(__name__)
@@ -81,6 +82,31 @@ def notes():
     conn.close()
 
     return render_template("notes.html", notes=notes)
+
+@app.route("/notes/create", methods=["GET","POST"])
+def create():
+    if "user_id" not in session:
+        return redirect("/login")
+
+    user_id = session["user_id"]
+
+    if request.method == "POST":
+        title = request.form.get("title")
+        content = request.form.get("content")
+        created_at = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+        conn = sqlite3.connect("database.db")
+        cursor = conn.cursor()
+
+        query = "INSERT INTO notes (user_id, title, content, created_at) VALUES (?, ?, ?, ?)"
+        data = [user_id, title, content, created_at]
+        cursor.execute(query, data)
+        conn.commit()
+        conn.close()
+
+        return redirect("/notes")
+
+    return render_template("create.html")
 
 
 if __name__ == "__main__":
